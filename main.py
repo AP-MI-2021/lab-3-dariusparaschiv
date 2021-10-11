b87c1faf68e34d50acf1779db1048fe70994810e
dariusparaschiv <91637941+dariusparaschiv@users.noreply.github.com>
2021-10-11 21:33:00 +0300

    lab3

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 """
 1. Toate numerele sunt pătrate perfecte.
   - Funcția de calcul: **get_longest_all_perfect_squares(lst: list[int]) -> list[int]**
+
 13.	Toate numerele sunt formate din cifre prime.
   - Funcția de calcul: **get_longest_prime_digits(lst: list[int]) -> list[int]**
+
+12. Toate numerele același număr de divizori.
+  - Funcția de calcul: get_longest_same_div_count(lst: list[int]) -> list[int]
 """
 
 import math
@@ -103,25 +107,90 @@ def problem_2(lst):
     print(get_longest_prime_digits(lst))
 
 
+def div_count(t):
+    if t == 1:
+        return 1
+    elif t == 2:
+        return 2
+    else:
+        k = 0
+        i = 1
+        while i <= t:
+            if t % i == 0:
+                k = k + 1
+            i = i + 1
+    return k
+
+
+def all_div_count(lst):
+    d = div_count(lst[0])
+    for x in lst:
+        if div_count(x) != d:
+            return False
+    return True
+
+
+def test_numar_divizori():
+    assert div_count(1) == 1
+    assert div_count(2) == 2
+    assert div_count(9) == 3
+    assert div_count(20) == 6
+
+
+def get_longest_same_div_count(lst):
+    """
+    determina cea mai lunga subsecventa de nr. cu acelasi nr de divizori
+    :param lst: lista de nr. intregi
+    :return: cea mai lunga subsecventa de nr. cu acelasi nr de divizori din lst
+    """
+    longest_subsequence = []
+    for i in range(len(lst)):
+        for j in range(i, len(lst)):
+            if all_div_count(lst[i:j + 1]) and len(lst[i:j + 1]) > len(longest_subsequence):
+                longest_subsequence = lst[i:j + 1]
+    return longest_subsequence
+
+
+def test_get_longest_same_div_count():
+    assert get_longest_same_div_count([2]) == [2]
+    assert get_longest_same_div_count([9, 8, 6]) == [8, 6]
+    assert get_longest_same_div_count([2, 20, 48, 5, 23]) == [5, 23]
+
+
+def read_list():
+    lst = []
+    given_sting = input("Dati o lista cu numerele separate prin virgula ")
+    numbers_as_string = given_sting.split(",")
+    for x in numbers_as_string:
+        lst.append(int(x))
+    return lst
+
+
+def problem_3(lst):
+    test_numar_divizori()
+    test_get_longest_same_div_count()
+    print(get_longest_same_div_count(lst))
+
+
 def print_menu():
-    print("1.Citire date.")
-    print("2.Determinare cea mai lungă subsecvență cu proprietatea 1.")
-    print("3.Determinare cea mai lungă subsecvență cu proprietatea 2.")
+    print("1.Determinare cea mai lungă subsecvență cu proprietatea 1.")
+    print("2.Determinare cea mai lungă subsecvență cu proprietatea 2.")
+    print("3.Determinare cea mai lungă subsecvență cu proprietatea 3.")
     print("4.Iesire")
 
 
 def main():
+    lst = read_list()
     while True:
         print_menu()
-        # lst = read_list()
         optiune = input("Alegeti o optiune ")
         if optiune == "1":
-            lst = read_list()
-        elif optiune == "2":
             problem_1(lst)
-        elif optiune == "3":
+        elif optiune == "2":
             problem_2(lst)
-        elif optiune == "x":
+        elif optiune == "3":
+            problem_3(lst)
+        elif optiune == "4":
             break
         else:
             print("optiune gresita!")
